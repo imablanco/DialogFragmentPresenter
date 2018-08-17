9fcfd9496dc4863531d097ad0d4c70412167ae36
Álvaro Blanco Cabrero <alvaro.bl91@gmail.com>
2018-08-18 00:38:48 +0200

    Added samples

---
+++ app/src/main/java/ablanco/com/dialogframentpresentersample/ActivityForResult.kt
@@ -0,0 +1,12 @@
+package ablanco.com.dialogframentpresentersample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActivityForResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_for_result)
+    }
+}
--- app/src/main/java/ablanco/com/dialogframentpresentersample/MainActivity.kt
+++ app/src/main/java/ablanco/com/dialogframentpresentersample/MainActivity.kt
@@ -1,19 +1,29 @@
 package ablanco.com.dialogframentpresentersample
 
 import ablanco.com.dialogfragmentpresenter.DialogFragmentPresenterProviders
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btShow.setOnClickListener {
+            Handler().postDelayed({
+                DialogFragmentPresenterProviders.of(this)?.show(SampleDialogFragment())
+            }, 5000)
+        }
+        btStartForResult.setOnClickListener {
+            startActivityForResult(Intent(this, ActivityForResult::class.java), 44444)
+        }
+    }
 
-        Handler().postDelayed({
-            DialogFragmentPresenterProviders.of(this).show(SampleDialogFragment())
-            DialogFragmentPresenterProviders.of(this).show(SampleDialogFragment())
-        }, 2000)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        DialogFragmentPresenterProviders.of(this)?.show(SampleDialogFragment())
     }
 }
